c8ac9ccc049c61173bb3bbf6c0ef774399998b2c
lix <lancer.k.lix@gmail.com>
2017-10-16 11:55:52 +0800

    add Android Study HTML record

---
+++ Android HTML/Android resorce packing and parsing/Android应用程序资源的编译和打包过程分析 - 老罗的Android之旅 - 51CTO技术博客_files/ajs.php
@@ -0,0 +1,3 @@
+var OX_ff44aeaf = '';
+OX_ff44aeaf += "<"+"div id=\'beacon_912d834f12\' style=\'position: absolute; left: 0px; top: 0px; visibility: hidden;\'><"+"img src=\'//gg3.51cto.com/www/delivery/lg.php?bannerid=3687&amp;campaignid=804&amp;zoneid=460&amp;loc=1&amp;referer=http%3A%2F%2Fshyluo.blog.51cto.com%2F5725845%2F1229261&amp;cb=912d834f12\' width=\'0\' height=\'0\' alt=\'\' style=\'width: 0px; height: 0px;\' /><"+"/div>\n";
+document.write(OX_ff44aeaf);
